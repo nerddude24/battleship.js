98b9fbd86be774b06f5c77cbd37e4506ce2d4b81
nerddude <30808467+nerddude24@users.noreply.github.com>
2024-09-16 20:47:38 +0100

    feat: event handler

---
+++ src/events.js
@@ -0,0 +1,35 @@
+const EventHandler = (() => {
+	_events = {};
+
+	function sub(eventName, fn) {
+		_events[eventName] = _events[eventName] || [];
+		_events[eventName].push(fn);
+	}
+
+	function unsub(eventName, fn) {
+		if (!_events[eventName]) return;
+
+		for (let i = 0; i < _events[eventName].length; i++) {
+			if (_events[eventName][i] === fn) {
+				_events[eventName].splice(i, 1);
+				break;
+			}
+		}
+	}
+
+	function emit(eventName, data) {
+		if (!_events[eventName]) return;
+
+		_events[eventName].forEach((fn) => {
+			fn(data);
+		});
+	}
+
+	return {
+		sub,
+		unsub,
+		emit,
+	};
+})();
+
+module.exports = EventHandler;
