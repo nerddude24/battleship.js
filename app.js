ef238759fa9ccdf66bd5326b7b5ec08d6a4ce959
nerddude <30808467+nerddude24@users.noreply.github.com>
2024-09-16 12:58:19 +0100

    feat: ship class with tests

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-console.log("Starting...");
+class Ship {
+	constructor(length) {
+		this._len = length;
+		this._hits = 0;
+	}
+
+	isSunk() {
+		return this._len - this._hits <= 0;
+	}
+
+	hit() {
+		this._hits = this._hits + 1;
+	}
+}
+
+module.exports = {
+	Ship,
+};
--- app.test.js
+++ app.test.js
@@ -1,3 +1,25 @@
-it("jest is working", () => {
-	expect("Hi").toBe("Hi");
+const { Ship } = require("./app.js");
+
+describe("Ship class (length == 3).", () => {
+	const ship = new Ship(3);
+
+	afterEach(() => {
+		ship.hit();
+	});
+
+	it("Ship is not sunk at first.", () => {
+		expect(ship.isSunk()).toBe(false);
+	});
+
+	it("Hitting once doesn't sink.", () => {
+		expect(ship.isSunk()).toBe(false);
+	});
+
+	it("Hitting twice doesn't sink.", () => {
+		expect(ship.isSunk()).toBe(false);
+	});
+
+	it("Hitting thrice sinks the ship.", () => {
+		expect(ship.isSunk()).toBe(true);
+	});
 });
